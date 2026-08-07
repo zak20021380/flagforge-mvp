74fb646e5410dfa1a547c638939798f1e5029e8d
Jakson mova <chatgpt20021380@gmail.com>
2026-08-07 10:57:53 +0330

    hghfghytyt

--- src/game/unitManager.ts
+++ src/game/unitManager.ts
@@ -206,7 +206,6 @@ export class UnitManager {
       kind,
       team,
       id,
-      (mesh) => this.shadows.addShadowCaster(mesh),
     );
     rig.setEnabled(false);
     for (const mesh of rig.root.getChildMeshes()) {
